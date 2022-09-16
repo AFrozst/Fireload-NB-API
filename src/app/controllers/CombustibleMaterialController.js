babcf6da07e280b67a885a3c3b5e2d07f2e17bd7
Allen-Vargas <allenbfox@gmial.com>
2022-09-15 20:43:32 -0400

    Endpoint gets for Combustible Material Entity

---
+++ src/app/controllers/CombustibleMaterialController.js
@@ -0,0 +1,47 @@
+"use strict";
+const { CombustibleMaterial } = require("../models");
+
+const CombustibleMaterialController = {
+  getCombustibleMaterials: async (req, res) => {
+    try {
+      let combustibleMaterials = await CombustibleMaterial.findAll({
+        raw: true,
+        nest: true,
+      });
+      return res.status(200).send({
+        data: combustibleMaterials,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const errorMessage = error.message || "Internal Server Error";
+      return res.status(status).send({ errorMessage });
+    }
+  },
+
+  getCombustibleMaterial: async (req, res) => {
+    try {
+      const { id } = req.params;
+      console.log(req);
+      let combustibleMaterial = await CombustibleMaterial.findOne({
+        where: { id },
+        raw: true,
+        nest: true,
+      });
+
+      if (!combustibleMaterial) {
+        return res.status(404).send({
+          errorMessage: "Combustible Material not found",
+        });
+      }
+      return res.status(200).send({
+        data: combustibleMaterial,
+      });
+    } catch (error) {
+      const status = error.status || 500;
+      const errorMessage = error.message || "Internal Server Error";
+      return res.status(status).send({ errorMessage });
+    }
+  },
+};
+
+module.exports = CombustibleMaterialController;
